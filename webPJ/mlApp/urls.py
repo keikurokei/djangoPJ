70bceb3bcd6a5d7d41596f10e4c3885fc8956964
keikurokei <keiichi,sub0702@gmail.com>
2019-04-29 06:53:21 +0900

    Adding Image upload function

--- webPJ/mlApp/urls.py
+++ webPJ/mlApp/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from . import views
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^$', 
         TemplateView.as_view(template_name='hello.html'),
         name='hello'),
+     url(r'^index/$', views.index, name='index'),       
 ]
--- webPJ/mlApp/views.py
+++ webPJ/mlApp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PhotoForm
+from .models import Photo
 
-# Create your views here.
+def index(req):
+    if req.method == 'GET':
+        return render(req, 'index.html', {
+            'form': PhotoForm(),
+            'photos': Photo.objects.all(), # ここを追加
+        })
+
+    elif req.method == 'POST':                                                     
+        form = PhotoForm(req.POST, req.FILES)
+        if not form.is_valid():
+            raise ValueError('invalid form')
+
+        photo = Photo()
+        photo.image = form.cleaned_data['image']
+        photo.save()
+
+        return redirect('/mlApp/index')
