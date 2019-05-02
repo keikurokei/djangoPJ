fd573ea0382828e13d190ccaa6f08c61d4acf227
keikurokei <keiichi,sub0702@gmail.com>
2019-05-02 23:27:56 +0900

    Saving file with uuid

--- webPJ/mlApp/models.py
+++ webPJ/mlApp/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+import uuid,os
+
+def get_image_path(self,filename):
+    """カスタマイズした画像パスを取得する.
+
+    :param filename: 元ファイル名
+    :return: カスタマイズしたファイル名を含む画像パス
+    """
+    prefix = 'mlApp/'
+    name = str(uuid.uuid4()).replace('-', '')
+    extension = os.path.splitext(filename)[-1]
+    return prefix + name + extension    
 
 class Photo(models.Model):
+    image = models.ImageField(upload_to=get_image_path)
 
-    image = models.ImageField(upload_to='mlApp')
--- webPJ/webPJ/settings.py
+++ webPJ/webPJ/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mlApp',
+    'django_cleanup',
 ]
 
 MIDDLEWARE = [
